c8d1ac88a49a0eeee5ce9bec3068f69ff75e978a
bhhoang818 <88428487+bhhoang818@users.noreply.github.com>
2022-06-07 18:08:41 +0700

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,32 @@
 window.onload = () => {
     initSplide();
-    const horizontalSections = gsap.utils.toArray('section.horizontal')
-
-    horizontalSections.forEach(function (sec, i) {
-
-        var thisPinWrap = sec.querySelector('.pin-wrap');
-        var thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
-
-        var getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
-
-        gsap.fromTo(thisAnimWrap, {
-            x: () => thisAnimWrap.classList.contains('to-right') ? 0 : getToValue()
-        }, {
-            x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0,
-            ease: "none",
-            scrollTrigger: {
-                trigger: sec,
-                start: "top top",
-                end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
-                pin: thisPinWrap,
-                invalidateOnRefresh: true,
-                //anticipatePin: 1,
-                scrub: true,
-                //markers: true,
-            }
-        });
-
-    });
 }
 const initSplide = () => {
-    new Splide('#splide', {
-        type: 'loop',
-        grid: {
-            rows: 2,
-            cols: 3,
-            gap: {
-                row: '1rem',
-                col: '1.5rem',
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".mySwiper .swiper-button-next",
+            prevEl: ".mySwiper .swiper-button-prev",
+        },
+    });
+    var swiper = new Swiper(".bannerSwiper", {
+        speed: 3500,
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+                shadow: true,
+                translate: ["-150%", 0, -500],
+            },
+            next: {
+                shadow: true,
+                translate: ["150%", 0, -500],
             },
         },
-    }).mount(window.splide.Extensions);
+        autoplay: {
+            delay: 3000
+        },
+    });
 }
